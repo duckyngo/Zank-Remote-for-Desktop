e28a479997e706412196cc617ca80133c0e0791b
NGO DUC KY <kynd@aithe.io>
2021-07-06 01:07:50 +0900

    Ajust UI

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import os
 import socket
 from enum import Enum
-from functools import partial
 
 import pyautogui
 import time
-import _thread
 import sys
 
 from PyQt5.QtGui import QIcon, QPalette, QPixmap, QImage, QCursor, QFont
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication, QGridLayout, QVBoxLayout, QSystemTrayIcon, QMenu,
-                             QAction, QHBoxLayout, QLabel, QSizePolicy)
-from PyQt5.QtCore import QThread, QObject, pyqtSignal
+                             QAction, QHBoxLayout, QLabel, QSizePolicy, QTabWidget, QMainWindow)
+from PyQt5.QtCore import QThread, QObject, pyqtSignal, pyqtSlot
 
 import qrcode
 from PIL import Image
@@ -58,12 +55,12 @@ def get_platform_type():
 def generate_qr_code():
     # taking image which user wants
     # in the QR code center
-    logo_link = 'app_icon.png'
+    logo_link = 'app_icon_rgb.jpg'
 
     logo = Image.open(logo_link)
 
     # taking base width
-    basewidth = 100
+    basewidth = 70
 
     # adjust image size
     wpercent = (basewidth / float(logo.size[0]))
@@ -86,12 +83,12 @@ def generate_qr_code():
     QRcolor = 'Black'
 
     # adding color to QR code
-    QRimg = QRcode.make_image(
-        fill_color=QRcolor, back_color="white").convert('RGB')
+    QRimg = QRcode.make_image().convert('RGB')
 
     # set size of QR code
     pos = ((QRimg.size[0] - logo.size[0]) // 2,
            (QRimg.size[1] - logo.size[1]) // 2)
+
     QRimg.paste(logo, pos)
 
     return QRimg
@@ -99,10 +96,16 @@ def generate_qr_code():
 
 def pil2pixmap(im):
     if im.mode == "RGB":
-        pass
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
     elif im.mode == "L":
         im = im.convert("RGBA")
-    data = im.convert("RGBA").tobytes()
+
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
     qim = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
     pixmap = QPixmap.fromImage(qim)
     return pixmap
@@ -270,7 +273,7 @@ class TCPWorker(QObject):
         self.continue_run = False  # set the run condition to false on stop
 
 
-class ShowIPText(QWidget):
+class ShowIPWindow(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -278,25 +281,31 @@ class ShowIPText(QWidget):
         # Buttons:
 
         self.title_lable = QLabel('Your computer IP address is: ', self)
-        self.title_lable.setFont(QFont('Arial', 16))
+        self.title_label_font = QFont('Arial', 16)
+        self.title_label_font.setBold(True)
+        self.title_lable.setFont(self.title_label_font)
         self.title_lable.setAlignment(Qt.AlignCenter)
 
         self.ip_label = QLabel(hostIP, self)
-        self.ip_label.setFont(QFont('Arial', 24))
-        self.ip_label.setAlignment(Qt.AlignCenter)
+        self.ip_label_font = QFont('Arial', 26)
+        self.ip_label_font.setBold(True)
+        self.ip_label.setFont(self.ip_label_font)
         self.ip_label.setContentsMargins(10, 40, 20, 10)
+        self.ip_label.setAlignment(Qt.AlignCenter)
 
         qr_image = generate_qr_code()
+        qr_pixmap = pil2pixmap(qr_image)
         self.imageLabel = QLabel()
         self.imageLabel.setAlignment(Qt.AlignCenter)
         self.imageLabel.setContentsMargins(10, 30, 10, 30)
-        self.imageLabel.setPixmap(pil2pixmap(qr_image))
+        self.imageLabel.setPixmap(qr_pixmap)
 
         self.btn_stop = QPushButton('Done')
         self.btn_stop.resize(self.btn_stop.sizeHint())
         self.btn_stop.move(150, 50)
 
         # GUI title, size, etc...
+        self.setGeometry(250, 200, 300, 420)
         self.setWindowTitle('Zank Remote Desktop')
         self.ip_layout = QVBoxLayout()
         self.ip_layout.addWidget(self.title_lable)
@@ -305,49 +314,207 @@ class ShowIPText(QWidget):
         self.ip_layout.addWidget(self.btn_stop)
         self.setLayout(self.ip_layout)
 
-        self.btn_stop.clicked.connect(self.closeEvent)
+        self.btn_stop.clicked.connect(self.close)
 
-    def show_ip_window(self):
+    def show_top(self):
         self.show()
+        self.setWindowState(self.windowState() & Qt.WindowMinimized | Qt.WindowActive)
+        self.raise_()
+        # this will activate the window
+        self.activateWindow()
 
 
-class Gui(QWidget):
-    stop_signal = pyqtSignal()  # make a stop signal to communicate with the worker in another thread
+class ControlPanelMainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        # Thread:
+        self.title = 'Zank Remote Desktop'
+        self.left = 0
+        self.top = 0
+        self.width = 500
+        self.height = 300
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.table_widget = ControlPanelTabWidget(self)
+        self.setCentralWidget(self.table_widget)
+
+    def show_top(self):
+        self.show()
+        self.setWindowState(self.windowState() & Qt.WindowMinimized | Qt.WindowActive)
+        self.raise_()
+        # this will activate the window
+        self.activateWindow()
+
+
+class ControlPanelTabWidget(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.statusTab = QWidget()
+        self.settingTab = QWidget()
+        self.aboutTab = QWidget()
+
+        # Add tabs
+        self.tabs.addTab(self.statusTab, "Status")
+        # self.tabs.addTab(self.settingTab, "Setting")
+        self.tabs.addTab(self.aboutTab, "About")
+
+        # Create first tab
+
+        self.name_label = QLabel("Computer Name: " + hostName, self)
+        self.name_label_font = QFont('Arial', 20)
+        self.name_label_font.setBold(True)
+        self.name_label.setFont(self.name_label_font)
+        self.name_label.setContentsMargins(10, 40, 20, 10)
+        self.name_label.setAlignment(Qt.AlignLeft)
+
+        self.ip_label = QLabel("Computer IP Address: " + hostIP, self)
+        self.ip_label_font = QFont('Arial', 20)
+        self.ip_label_font.setBold(True)
+        self.ip_label.setFont(self.ip_label_font)
+        self.ip_label.setContentsMargins(10, 40, 20, 10)
+        self.ip_label.setAlignment(Qt.AlignLeft)
+
+        qr_image = generate_qr_code()
+        qr_pixmap = pil2pixmap(qr_image)
+        self.imageLabel = QLabel()
+        self.imageLabel.setAlignment(Qt.AlignCenter)
+        self.imageLabel.setContentsMargins(10, 30, 10, 30)
+        self.imageLabel.setPixmap(qr_pixmap)
+
+        self.statusTab.layout = QVBoxLayout(self)
+        self.statusTab.layout.addWidget(self.name_label)
+        self.statusTab.layout.addWidget(self.ip_label)
+        self.statusTab.layout.addWidget(self.imageLabel)
+        self.statusTab.setLayout(self.statusTab.layout)
+
+        # Create second tab
+
+        # Create third tab
+
+        self.appPixmap = QPixmap('app_icon_rgb.jpg')
+
+        self.appImageLabel = QLabel()
+        self.appImageLabel.setAlignment(Qt.AlignCenter)
+        self.scaled = self.appPixmap.scaled(self.appImageLabel.size(), Qt.KeepAspectRatio)
+        self.appImageLabel.setPixmap(self.scaled)
+        self.sp = self.appImageLabel.sizePolicy()
+        self.sp.setHorizontalPolicy(QSizePolicy.Maximum)
+        self.appImageLabel.setSizePolicy(self.sp)
+
+        self.app_name_label = QLabel("Zank Remote Desktop", self)
+        self.app_name_label_font = QFont('Arial', 20)
+        self.app_name_label_font.setBold(True)
+        self.app_name_label.setFont(self.app_name_label_font)
+        # self.app_name_label.setContentsMargins(10, 5, 20, 10)
+        self.app_name_label.setAlignment(Qt.AlignCenter)
+
+        self.app_slogan_label = QLabel("Control everything in your hand", self)
+        self.app_slogan_label_font = QFont('Arial', 18)
+        # self.app_slogan_label_font.setBold(True)
+        self.app_slogan_label.setFont(self.app_slogan_label_font)
+        # self.app_slogan_label.setContentsMargins(10, 40, 20, 10)
+        self.app_slogan_label.setAlignment(Qt.AlignCenter)
+
+        self.app_web_label = QLabel("www.zankremote.com", self)
+        self.app_web_label_font = QFont('Arial', 14)
+        self.app_web_label.setFont(self.app_web_label_font)
+        # self.app_slogan_label.setContentsMargins(10, 40, 20, 10)
+        self.app_web_label.setAlignment(Qt.AlignCenter)
+
+        self.aboutTab.layout = QVBoxLayout(self)
+        self.aboutTab.layout.addWidget(self.appImageLabel)
+        self.aboutTab.layout.addWidget(self.app_name_label)
+        self.aboutTab.layout.addWidget(self.app_slogan_label)
+        self.aboutTab.layout.addWidget(self.app_web_label)
+        self.aboutTab.setLayout(self.aboutTab.layout)
+
+        # Add tabs to widget
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
+
+class ZankRemoteApplication(QApplication):
+
+    stop_signal = pyqtSignal()  # make a stop signal to communicate with the worker in another thread
+
+    def __init__(self, args):
+        """ In the constructor we're doing everything to get our application
+            started, which is basically constructing a basic QApplication by
+            its __init__ method, then adding our widgets and finally starting
+            the exec_loop."""
+        QApplication.__init__(self, args)
+
         self.thread = QThread()
         self.tcp_thread = QThread()
         self.worker = UDPWorker()
         self.tcp_worker = TCPWorker()
 
-        # self.initUI()
+        self.app_icon = QIcon("app_icon_round.png")
+        self.showIpWindow = ShowIPWindow()
+        self.controlPanelMainWindow = ControlPanelMainWindow()
+        self.icon = QIcon("app_icon_round.png")
 
-    def initUI(self):
-        # Buttons:
-        self.btn_start = QPushButton('Start')
-        self.btn_start.resize(self.btn_start.sizeHint())
-        self.btn_start.move(50, 50)
-        self.btn_stop = QPushButton('Stop')
-        self.btn_stop.resize(self.btn_stop.sizeHint())
-        self.btn_stop.move(150, 50)
 
-        # GUI title, size, etc...
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Zank Remote Desktop')
-        self.layout = QGridLayout()
-        self.layout.addWidget(self.btn_start, 0, 0)
-        self.layout.addWidget(self.btn_stop, 0, 50)
-        self.setLayout(self.layout)
+        self.addWidgets()
 
-        # Start Button action:
-        self.btn_start.clicked.connect(self.start_server)
+    def addWidgets(self):
+        """ In this method, we're adding widgets and connecting signals from
+            these widgets to methods of our class, the so-called "slots"
+        """
+        # Set app icon
+        self.setWindowIcon(self.app_icon)
 
-        # Stop Button action:
-        self.btn_stop.clicked.connect(self.stop_thread)
+        # Create the icon
 
-        self.show()
+        # Create the tray
+        self.tray = QSystemTrayIcon()
+        self.tray.setIcon(self.icon)
+        self.tray.setVisible(True)
+
+        # Create the menu
+        self.menu = QMenu()
+
+        self.see_tutorials = QAction("Tutorials")
+        self.menu.addAction(self.see_tutorials)
+
+        self.show_ip_window = QAction("Show IP Address")
+        self.show_ip_window.triggered.connect(self.showIpWindow.show_top)
+        self.menu.addAction(self.show_ip_window)
+
+        self.restart = QAction("Restart")
+        self.menu.addAction(self.restart)
+
+        self.control_panel = QAction("Control Panels")
+        self.control_panel.triggered.connect(self.controlPanelMainWindow.show_top)
+        self.menu.addAction(self.control_panel)
+
+        # Add a Quit option to the menu.
+        self.quit_action = QAction("Quit Zank Remote")
+        self.quit_action.triggered.connect(self.stop_thread)
+        self.quit_action.triggered.connect(self.quit)
+        self.menu.addAction(self.quit_action)
+
+        # Add the menu to the tray
+        self.tray.setContextMenu(self.menu)
+
+        self.start_server()
+
+    def slotSayHello(self):
+        """ This is an example slot, a method that gets called when a signal is
+            emitted """
+        print ("Hello, World!")
 
     def start_server(self):
         self.stop_signal.connect(self.worker.stop)  # connect stop signal to worker stop method
@@ -379,54 +546,8 @@ class Gui(QWidget):
         print("Stop signal emit")
         self.stop_signal.emit()  # emit the finished signal on stop
 
-
 if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
+    app = ZankRemoteApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
-
-    # Set app icon
-    app_icon = QIcon("app_icon.png")
-    app.setWindowIcon(app_icon)
-
-    gui = Gui()
-    show_ip = ShowIPText()
-
-    gui.start_server()
-
-    # Create the icon
-    icon = QIcon("app_icon.png")
-
-    # Create the tray
-    tray = QSystemTrayIcon()
-    tray.setIcon(icon)
-    tray.setVisible(True)
-
-    # Create the menu
-    menu = QMenu()
-
-    see_tutorials = QAction("Tutorials")
-    menu.addAction(see_tutorials)
-
-    show_ip_window = QAction("Show IP Address")
-    show_ip_window.triggered.connect(show_ip.show_ip_window)
-    menu.addAction(show_ip_window)
-
-    restart = QAction("Restart")
-    menu.addAction(restart)
-
-    control_panel = QAction("Control Panels")
-    control_panel.triggered.connect(gui.initUI)
-    menu.addAction(control_panel)
-
-    # Add a Quit option to the menu.
-    quit = QAction("Quit Zank Remote")
-    quit.triggered.connect(gui.stop_thread)
-    quit.triggered.connect(app.quit)
-    menu.addAction(quit)
-
-    # Add the menu to the tray
-    tray.setContextMenu(menu)
-
-    # app.exec_()
-    sys.exit(app.exec_())
+    app.exec_()
+    # sys.exit(app.exec_())
